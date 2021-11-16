4bf5ce0b8c37f27790f961c5422c0e2da8174cd2
Daniel Bukowski <danioArch@protonmail.com>
2021-11-16 21:43:55 +0100

    Add logos images and ColorPallets class

---
+++ src/main/java/colorSchemes/ColorPallets.java
@@ -0,0 +1,33 @@
+package colorSchemes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ColorPallets
+{
+    public final String backgroundColor = "background";
+    public final String backgroundSecondColor = "backgroundSecond";
+    public final String textColor = "text";
+    public final String buttonColor = "button";
+    public final String warningColor = "warning";
+
+    Map<String, String> colorLightMap = new HashMap<String, String>();
+    Map<String, String> colorDarkMap = new HashMap<String, String>();
+
+    public ColorPallets()
+    {
+        colorLightMap.put(backgroundColor, "#2C3E50");
+        colorLightMap.put(backgroundSecondColor, "#34495E");
+        colorLightMap.put(textColor, "#BDC3C7");
+        colorLightMap.put(buttonColor, "#2980B9");
+        colorLightMap.put(warningColor, "#C0392B");
+
+        colorDarkMap.put(backgroundColor, "#ECF0F1");
+        colorDarkMap.put(backgroundSecondColor, "#BDC3C7");
+        colorDarkMap.put(textColor, "#34495E");
+        colorDarkMap.put(buttonColor, "#3498DB");
+        colorDarkMap.put(warningColor, "#E74C3C");
+    }
+
+
+}
