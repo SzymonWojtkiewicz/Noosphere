7aa423df72a90b15ea992a346b6e23f0263f6f7f
Drobniak <piotrdrobina@gmail.com>
2022-01-03 22:58:34 +0100

    22.58 03.01.2022 creating interface and methods: view, checkifadmin, delete

---
+++ src/main/java/Database/Database.java
@@ -0,0 +1,19 @@
+package Database;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public interface Database {
+    public void connectToMysql();
+    public void connectToDatabase();
+    public void createDatabase();
+    public void createUsersTable();
+    public void createVideosTable();
+    public void createAccount(String username, String email, String password, boolean administrator);
+    public void upgradeToAdmin(String username);
+    public void downgradeToUser(String username);
+    public void displayAccountName(String username);
+    public void displayAccount(String username);
+    public void checkIfAdmin(String username);
+    public void deleteAccount(String username);
+}
