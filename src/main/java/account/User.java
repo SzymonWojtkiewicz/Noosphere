d3f04b5d07bacf79c7f2a211f1cbf82b48317711
Szymon Wojtkiewicz <swojtkiewicz@vp.pl>
2022-01-31 14:45:57 +0100

    1445 31.01.22 SW

--- src/main/java/account/User.java
+++ src/main/java/account/User.java
@@ -4,10 +4,13 @@ public class User {
     private long id;
     private String firstName;
     private String lastName;
-    byte[] password; // place holder HASH PASSWORD
+    //private byte[] password; // place holder HASH PASSWORD
+    private String password;
     private String email;
     private Boolean isAdmin;
     private Boolean isMegaAdmin;
+
+
 
     public User(){
 
---
+++ src/main/java/security/HashingPassword.java
@@ -0,0 +1,10 @@
+package security;
+
+public class HashingPassword {
+    public static String incript(String password){
+        return password;
+    }
+    public static Boolean check (String password){
+        return true;
+    }
+}
